8428ce7a498940dc6f24a15955e386115acd654b
Antorkarmokar28 <antorkarmokar28@gmail.com>
2024-11-27 08:33:17 +0000

    This server is connect to the mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 const app = express();
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello Bi-Cycle-store-B4A2V4 server');
 });
 export default app;
